670bfcd563da1bf50e76dca702d7b440c4632f15
pjanthony2001 <104015990+pjanthony2001@users.noreply.github.com>
2023-06-24 06:21:58 +0800

    Added example service client
    - added functionality for service registry and discovery for server side
    - have to configure client side utilization
    - edited go.mod for client side to change netpoll v0.4.0 -> v0.3.2
    - set up nacos server for service registry

--- kitex_server/server.go
+++ kitex_server/server.go
@@ -8,9 +8,44 @@ import (
 
 	"github.com/cloudwego/kitex/pkg/generic"
 	"github.com/cloudwego/kitex/server/genericserver"
+
+	"github.com/cloudwego/kitex/pkg/rpcinfo"
+	"github.com/cloudwego/kitex/server"
+	"github.com/kitex-contrib/registry-nacos/example/hello/kitex_gen/api"
+	"github.com/kitex-contrib/registry-nacos/registry"
+	"github.com/nacos-group/nacos-sdk-go/clients"
+	"github.com/nacos-group/nacos-sdk-go/common/constant"
+	"github.com/nacos-group/nacos-sdk-go/vo"
 )
 
 func main() {
+
+	// the nacos server config
+	sc := []constant.ServerConfig{
+		*constant.NewServerConfig("127.0.0.1", 8848),
+	}
+
+	// the nacos client config
+	cc := constant.ClientConfig{
+		NamespaceId:         "public",
+		TimeoutMs:           100000,
+		NotLoadCacheAtStart: true,
+		LogDir:              "/tmp/nacos/log",
+		CacheDir:            "/tmp/nacos/cache",
+		LogLevel:            "info",
+		// more ...
+	}
+
+	cli, err := clients.NewNamingClient(
+		vo.NacosClientParam{
+			ClientConfig:  &cc,
+			ServerConfigs: sc,
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
 	// Parse IDL with Local Files
 	// YOUR_IDL_PATH thrift file path,eg: ./idl/example.thrift
 	p, err := generic.NewThriftFileProvider("../idl/example_service.thrift")
@@ -21,10 +56,16 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	svr := genericserver.NewServer(new(GenericServiceImpl), g)
+	svr := genericserver.NewServer(
+		new(GenericServiceImpl),
+		g,
+		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: "Hello"}),
+		server.WithRegistry(registry.NewNacosRegistry(cli)))
+
 	if err != nil {
 		panic(err)
 	}
+
 	err = svr.Run()
 	if err != nil {
 		fmt.Println("this is the error runtime")
@@ -44,4 +85,13 @@ func (g *GenericServiceImpl) GenericCall(ctx context.Context, method string, req
 	}
 	Message := requestData["Msg"].(string)
 	return "{\"Msg\" : \"" + Message + "\"}", err
+}
+
+type HelloImpl struct{}
+
+func (h *HelloImpl) Echo(_ context.Context, req *api.Request) (resp *api.Response, err error) {
+	resp = &api.Response{
+		Message: req.Message,
+	}
+	return
 }
