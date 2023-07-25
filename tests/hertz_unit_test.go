1a27a433da7845da27cd438d8d89dee540e19fe0
pjanthony2001 <104015990+pjanthony2001@users.noreply.github.com>
2023-07-25 19:43:51 +0800

    testing functionality added
    Added a range of tests:
    
    Hertz Server:
    - Ping Server
    
    Kitex Server:
    1. Without Resolver:
    - empty msg
    - normal msg
    - empty msg with additional json
    - normal msg with additional json
    
    2. With Resolver:
    - empty msg
    - normal msg
    - empty msg with additional json
    - normal msg with additional json
    
    Integration (Hertz + Kitex Server):
    - empty req
    - msg req
    - flag req
    - msg + flag req
    - empty req with additional json
    - msg + flag req with additional json
    
    Nacos Server:
    - Service Discovery Test

--- tests/hertz_unit_test.go
+++ tests/hertz_unit_test.go
@@ -1,3 +1,51 @@
+package tests
+
 import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"strings"
 	"testing"
 )
+
+const hertzURL = "http://127.0.0.1:8080"
+
+func TestHertz(t *testing.T) {
+	var tests = []struct {
+		name  string
+		query string
+		data  string
+		want  string
+	}{
+		{"/ping should be pong", "/ping", "", "pong"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			reqBody := strings.NewReader(tt.data)
+			req, err := http.NewRequest(http.MethodGet, hertzURL+tt.query, reqBody)
+			if err != nil {
+				t.Fatalf("Error from creating request %s", err.Error())
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Fatalf("Error from making request %s", err.Error())
+			}
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("Error from reading response %s", err.Error())
+			}
+
+			t.Log(string(body))
+			var sb map[string]interface{}
+			json.Unmarshal(body, &sb)
+
+			if sb["message"] != tt.want {
+				t.Errorf("got %s, want %s", sb, tt.want)
+			}
+		})
+	}
+}
--- tests/integration_test.go
+++ tests/integration_test.go
@@ -1,3 +1,67 @@
+package tests
+
 import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
 	"testing"
 )
+
+func TestIntegration(t *testing.T) {
+	var tests = []struct {
+		name             string
+		url              string
+		wantMessage      string
+		wantFlag         int32
+		additionalInputs string
+	}{
+		{"empty json should be empty", "/echo/query", "", 0, ""},
+		{"message in json should be message", "/echo/query", "Message", 0, ""},
+		{"flag in json should be flag", "/echo/query", "", 300, ""},
+		{"message and flag in json should be message and flag json", "/echo/query", "Hello", 211, ""},
+		{"Additional inputs with empty request should have no effect", "/echo/query", "", 0, "Additional"},
+		{"Additional inputs with full request should have no effect", "/echo/query", "Hallo", 212, "Additional"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			data, err := json.Marshal(map[string]interface{}{"Message": tt.wantMessage, "Flag": tt.wantFlag, "Additional": tt.additionalInputs})
+			if err != nil {
+				t.Fatalf("Error from creating json data %s", err.Error())
+			}
+			reqBody := bytes.NewReader(data)
+			req, err := http.NewRequest(http.MethodGet, hertzURL+tt.url, reqBody)
+			if err != nil {
+				t.Fatalf("Error from creating request %s", err.Error())
+			}
+
+			req.Header.Set("Content-Type", "application/json")
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Fatalf("Error from making request %s", err.Error())
+			}
+
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				t.Fatalf("Error from reading response %s", err.Error())
+			}
+
+			t.Log(string(body))
+			var sb map[string]interface{}
+			json.Unmarshal(body, &sb)
+
+			if sb["Message"] != tt.wantMessage {
+				t.Errorf("got %s, want %s", sb["Message"], tt.wantMessage)
+			}
+			if int32(sb["Flag"].(float64)) != tt.wantFlag {
+				t.Errorf("got %f, want %d", sb["Flag"].(float64), tt.wantFlag)
+			}
+			if len(sb) != 2 {
+				t.Errorf("got %d, want %d", len(sb), 2)
+			}
+		})
+	}
+}
